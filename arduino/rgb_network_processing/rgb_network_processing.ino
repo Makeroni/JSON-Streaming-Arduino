90fad1d9114bc22920b54df77e35d39b91427c4c
berro78 <berro78@gmail.com>
2014-02-14 15:38:06 +0100

    Commit 5
    Arduino, Processing, PHP running OK.

--- arduino/rgb_network_processing/rgb_network_processing.ino
+++ arduino/rgb_network_processing/rgb_network_processing.ino
@@ -1,33 +1,31 @@
-/*
-  Web client
- 
- Circuit:
- * Ethernet shield attached to pins 10, 11, 12, 13
- 
- */
-
 #include <SPI.h>
 #include <Ethernet.h>
 
 // Enter a MAC address for your controller below.
 // Newer Ethernet shields have a MAC address printed on a sticker on the shield
-byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+byte mac[] = { 
+  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 // if you don't want to use DNS (and reduce your sketch size)
 // use the numeric IP instead of the name for the server:
-char server[] = "www.pruebasberro.tk";    // name address for the server (using DNS)
+char server[] = "siminelaki.org";    // name address for the server (using DNS)
 
 // Set the static IP address to use if the DHCP fails to assign
 IPAddress ip(192,168,0,178);
 
 // Initialize the Ethernet client library
-// with the IP address and port of the server 
+// with the IP address and port of the server
 // that you want to connect to (port 80 is default for HTTP):
 EthernetClient client;
 
 void setup() {
- // Open serial communications and wait for port to open:
-  Serial.begin(9600);
-   while (!Serial) {
+
+  pinMode(3,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
+
+  // Open serial communications and wait for port to open:
+  Serial.begin(115200);
+  while (!Serial) {
     ; // wait for serial port to connect. Needed for Leonardo only
   }
 
@@ -46,24 +44,56 @@ void setup() {
   if (client.connect(server, 80)) {
     //Serial.println("connected");
     // Make a HTTP request:
-    client.println("GET /arduino/rgb_network/?q=arduino HTTP/1.1");
-    client.println("Host: www.pruebasberro.tk");
+    client.println("GET /noname/arduino-test.php HTTP/1.1");
+    client.println("Host: siminelaki.org");
     client.println("Connection: close");
     client.println();
-  } 
+  }
   else {
     // if you didn't get a connection to the server:
     //Serial.println("connection failed");
   }
 }
 
+char buffer[200];
+int i=0;
+int id, value;
+int a_color[10];
+
 void loop()
 {
-  // if there are incoming bytes available 
+  // if there are incoming bytes available
   // from the server, read them and print them:
-  if (client.available()) {
-    char c = client.read();
-    Serial.print(c);
+  while (client.available()) {
+    buffer[i] = client.read();
+    if( buffer[i] == '\n' )
+    {
+      buffer[i] = '\0';
+      if( i > 0 && buffer[i-1] == ',' )
+      {
+        buffer[12] = '\0';
+        id = atoi(buffer+8);
+
+        buffer[27] = '\0';
+        value = atoi(buffer+23);
+
+
+        if( id==3 || id==5 || id==6 )
+        {
+          a_color[id]=value;
+          analogWrite(id, value);
+        }
+
+        Serial.print(a_color[3]);
+        Serial.print(",");   
+        Serial.print(a_color[5]);
+        Serial.print(",");   
+        Serial.println(a_color[6]);      
+      }
+      i = -1;
+    }
+    i++;
+    //Serial.print(c);
   }
 
   // if the server's disconnected, stop the client:
@@ -71,7 +101,6 @@ void loop()
     //Serial.println();
     //Serial.println("disconnecting.");
     client.stop();
-
     // do nothing forevermore:
     while(true);
   }
---
+++ php/arduino-test.php
@@ -0,0 +1,57 @@
+<?
+# We start with the standard headers. PHP allows us this much
+header("Cache-Control: no-cache");
+header("Cache-Control: private");
+header("Pragma: no-cache");
+header("Content-type: text/plain");
+
+# Set this so PHP doesn't timeout during a long stream
+set_time_limit(0);
+
+# Disable Apache and PHP's compression of output to the client
+@apache_setenv('no-gzip', 1);
+@ini_set('zlib.output_compression', 0);
+
+# Set implicit flush, and flush all current buffers
+@ini_set('implicit_flush', 1);
+for ($i = 0; $i < ob_get_level(); $i++)
+    ob_end_flush();
+ob_implicit_flush(1);
+
+# The loop
+$i=0;
+while (true) {
+
+    $red   = sin($i*2*pi()/128 + 0) * 127 + 128;
+    $green = sin($i*2*pi()/128 + 2) * 127 + 128;
+    $blue  = sin($i*2*pi()/128 + 4) * 127 + 128;
+
+    $id = 3;
+    $value = sprintf("%03d", $red);
+    $id = sprintf("%03d", $id);
+    $msg = "{ \"id\" : ".$id.", \"value\" : ".$value." },\n";
+    $msg = str_replace(': 00', ':   ', $msg);
+    $msg = str_replace(': 0', ':  ', $msg);
+    echo $msg;
+
+    $id = 5;
+    $value = sprintf("%03d", $green);
+    $id = sprintf("%03d", $id);
+    $msg = "{ \"id\" : ".$id.", \"value\" : ".$value." },\n";
+    $msg = str_replace(': 00', ':   ', $msg);
+    $msg = str_replace(': 0', ':  ', $msg);
+    echo $msg;
+
+    $id = 6;
+    $value = sprintf("%03d", $blue);
+    $id = sprintf("%03d", $id);
+    $msg = "{ \"id\" : ".$id.", \"value\" : ".$value." },\n";
+    $msg = str_replace(': 00', ':   ', $msg);
+    $msg = str_replace(': 0', ':  ', $msg);
+    echo $msg;
+
+    $i=$i+2;
+    if($i==128){ $i=0; }
+    usleep(100000);
+}
+?>
