e1cb805410f3c5429359cd1351cbec1dee46fd66
B0RJA <borja.latorre@eead.csic.es>
2014-03-31 10:37:28 +0200

    Update emitter.php

--- emitter.php
+++ emitter.php
@@ -22,10 +22,12 @@ ob_implicit_flush(1);
 $i=0;
 while (true) {
 
+    # Rainbow gradient
     $red   = sin($i*2*pi()/128 + 0) * 127 + 128;
     $green = sin($i*2*pi()/128 + 2) * 127 + 128;
     $blue  = sin($i*2*pi()/128 + 4) * 127 + 128;
 
+    # JSON data for red color
     $id = 3;
     $value = sprintf("%03d", $red);
     $id = sprintf("%03d", $id);
@@ -34,6 +36,7 @@ while (true) {
     $msg = str_replace(': 0', ':  ', $msg);
     echo $msg;
 
+    # JSON data for green color
     $id = 5;
     $value = sprintf("%03d", $green);
     $id = sprintf("%03d", $id);
@@ -42,6 +45,7 @@ while (true) {
     $msg = str_replace(': 0', ':  ', $msg);
     echo $msg;
 
+    # JSON data for blue color
     $id = 6;
     $value = sprintf("%03d", $blue);
     $id = sprintf("%03d", $id);
@@ -50,8 +54,11 @@ while (true) {
     $msg = str_replace(': 0', ':  ', $msg);
     echo $msg;
 
+    # Advance gradient and loop
     $i=$i+2;
     if($i==128){ $i=0; }
+    
+    # 100ms sleep (10 messages per second)
     usleep(100000);
 }
 ?>
